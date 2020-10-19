c8e462441304f48a6cbf10d869779707497a19b6
DJ Viking <anders.swedishviking@gmail.com>
2020-10-18 22:13:24 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.85063506d0e6c168458597ee2ae566d7.js"
+  "/react-portfolio/precache-manifest.dca54ebbf16bca667f6d09d1f4f8de3e.js"
 );
 
 self.addEventListener('message', (event) => {
